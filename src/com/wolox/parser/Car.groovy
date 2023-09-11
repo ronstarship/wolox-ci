53f166f7365f68d4adba55192800c68fb0f996d7
ronstarship <ssonali.008@gmail.com>
2023-09-11 23:51:16 +0200

    import statement

--- src/com/wolox/parser/Car.groovy
+++ src/com/wolox/parser/Car.groovy
@@ -50,4 +50,90 @@ class Car {
 
         return projectConfiguration;
     }
+
+    static def parseEnvironment(def environment, def jenkinsEnvironment, def env) {
+        def config = [];
+
+        if (environment) {
+            config += environment.collect { k, v -> "${k}=${v}"};
+        }
+
+        if (jenkinsEnvironment) {
+            config += jenkinsEnvironment.collect { k -> "${k}=${env.getProperty(k)}"};
+        }
+
+        return config;
+    }
+
+    static def parseSteps(def yamlSteps) {
+        List<Step> steps = yamlSteps.collect { k, v ->
+            Step step = new Step(name: k)
+
+            // a step can have one or more commands to execute
+            v.each {
+                step.commands.add(it);
+            }
+            return step
+        }
+        return new Steps(steps: steps);
+    }
+
+    static def parseServices(def steps) {
+        def services = [];
+
+        steps.each {
+            def service = it.tokenize(':')
+            def version = service.size() == 2 ? service.get(1) : LATEST
+            def instance = getServiceClass(service.get(0).capitalize())?.newInstance()
+
+            services.add([service: instance, version: version])
+        };
+
+        services.add([service: new Base(), version: LATEST]);
+
+        return services
+    }
+
+    static def getServiceClass(def name) {
+        // TODO: Refactor this
+        switch(name) {
+            case "Postgres":
+                return Postgres
+                break
+            case "Postgis":
+                return Postgis
+                break
+            case "Redis":
+                return Redis
+                break
+            case "Mssql":
+                return Mssql
+                break
+            case "Mysql":
+                return Mysql
+                break
+            case "Mongodb":
+                return Mongodb
+                break
+            case "Elasticsearch":
+                return Elasticsearch
+                break
+        }
+    }
+
+    static def parseDockerfile(def config) {
+        if (!config || !config["dockerfile"]) {
+            return "Dockerfile";
+        }
+
+        return config["dockerfile"];
+    }
+
+    static def parseProjectName(def config) {
+        if (!config || !config["project_name"]) {
+            return "woloxci-project";
+        }
+
+        return config["project_name"];
+    }
 }
