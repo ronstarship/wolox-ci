c36537b6223b07e67ebcbfc52ba2abac99e9eb43
ronstarship <ssonali.008@gmail.com>
2023-09-11 22:41:16 +0200

    import statement

--- vars/ImportSRC.groovy
+++ vars/ImportSRC.groovy
@@ -1,10 +1,7 @@
 import com.wolox.parser.*;
 
 def call(String yamlName) {
-    def myCar = new Car(script)
-	myCar.numberOfDoors = 5
-	myCar.brand = "123"
-	myCar.model = "abc"
-	myCar.printOutCar()
+
 	def yaml = readYaml file: yamlName;
+	print(yaml.environment)
 }
