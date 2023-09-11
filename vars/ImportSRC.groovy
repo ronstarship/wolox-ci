1a25ec3656f9a1771c87bbbb927a4bb8a2475bd2
ronstarship <ssonali.008@gmail.com>
2023-09-11 23:54:19 +0200

    import statement

--- vars/ImportSRC.groovy
+++ vars/ImportSRC.groovy
@@ -4,7 +4,7 @@ import com.wolox.*;
 def call(String yamlName) {
 
 	def yaml = readYaml file: yamlName;
-	print(env.BUILD_ID);
+	print(env.BRANCH_NAME);
 	def buildNumber = Integer.parseInt(env.BUILD_ID);
 	ProjectConfiguration projectConfig = Car.parsec(yaml, env);
 	
