0f84af03f549277f7cc592b5f72ae2dd9c9ad84d
ronstarship <ssonali.008@gmail.com>
2023-09-13 20:19:17 +0200

    new

--- vars/woloxCi.groovy
+++ vars/woloxCi.groovy
@@ -30,8 +30,8 @@ def call(String yamlName) {
         closure([:]);
 
     // build the image specified in the configuration
-    def dockerImage = docker.build("-t $registry+:$BUILD_NUMBER --build-arg ARTIFACT_NAME=demo-$VERSION-SNAPSHOT.jar --build-arg APP_PORT=8080 "," --file ${projectConfig.dockerfile} .");
-
+    //def dockerImage = docker.build("-t $registry+:$BUILD_NUMBER --build-arg ARTIFACT_NAME=demo-$VERSION-SNAPSHOT.jar --build-arg APP_PORT=8080 "," --file ${projectConfig.dockerfile} .");
+    def dockerImage = docker.build registry + ":$BUILD_NUMBER --build-arg ARTIFACT_NAME=demo-$VERSION-SNAPSHOT.jar --build-arg APP_PORT=8080" + .
     def registryCredential = 'dockerhub'
 
     docker.withRegistry( '', registryCredential ) {
