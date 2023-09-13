0cb4cdabad3bcded37663e6dd799065bf7fd6792
ronstarship <ssonali.008@gmail.com>
2023-09-13 15:38:44 +0200

    new

--- vars/woloxCi.groovy
+++ vars/woloxCi.groovy
@@ -14,17 +14,19 @@ def call(String yamlName) {
     def imageName = projectConfig.dockerConfiguration.imageName().toLowerCase();
 
     // build the image specified in the configuration
-    def customImage = docker.build(imageName, "--file ${projectConfig.dockerfile} .");
+    def dockerImage = docker.build(imageName, "--file ${projectConfig.dockerfile} .");
+
+    dockerImage.push('latest')
 
     // adds the last step of the build.
     def closure = buildSteps(projectConfig);
-    print("closure1"+closure([0]))
+ 
     sh 'mvn clean install'
     // each service is a closure that when called it executes its logic and then calls a closure, the next step.
     projectConfig.services.each {
 
         closure = "${it.service.getVar()}"(projectConfig, it.version, closure);
-        print("closure2"+closure([1]))
+
     }
 
     // we execute the top level closure so that the cascade starts.
